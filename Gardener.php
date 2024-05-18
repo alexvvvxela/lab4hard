b5f7f83f5ffd4cde188bb976bc21f73ebaadb227
alexvvvxela <158572564+alexvvvxela@users.noreply.github.com>
2024-05-18 10:01:40 +0300

    Update Gardener.php

--- Gardener.php
+++ Gardener.php
@@ -2,35 +2,37 @@
 
 require_once 'tomatoBush.php';
 
-class Gardener {
+class Gardener
+{
     public $name;
     private $plant;
 
-    public function __construct($name, $plant) {
+    public function __construct($name, $plant)
+    {
         $this->name = $name;
         $this->plant = $plant;
     }
 
-    public function work() {
+    public function work(): void
+    {
         $this->plant->growAll();
-
     }
 
-    public function harvest(){
+    public function harvest(): void
+    {
         if ($this->plant->allAreRipe()) {
-            echo 'Урожай собран!'. PHP_EOL ;
+            echo 'Урожай собран!' . PHP_EOL;
 
             $this->plant->giveAwayAll();
         } else {
-            echo 'Помидорки еще не все созрели.'. PHP_EOL;
+            echo 'Помидорки еще не все созрели.' . PHP_EOL;
         }
     }
 
-    public static function KnowledgeBase() {
+    public static function KnowledgeBase(): void
+    {
 
         echo 'Справка по садоводству' . PHP_EOL;
         echo 'Ухаживайте за помидорками пока они не созрели' . PHP_EOL;
     }
 }
-
-?>
